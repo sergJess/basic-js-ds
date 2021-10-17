b560e8badd40b88d2b94321fd0dffdb768454154
sergJess <serg.fantasy@yandex.ru>
2021-10-17 20:18:21 +0300

    feat: add methods min, max, find in binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,26 +6,76 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node  {
+  constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+  }
+}
 module.exports = class BinarySearchTree {
-
+constructor(){
+  this.rootVal = null;
+}
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.rootVal){
+      return this.rootVal;
+    }
+   return null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add( data ) {
+    this.rootVal  = addWithin(this.rootVal, data);
+    function addWithin(node, data){
+
+      if(!node){
+        return new Node(data);
+      }
+      if(node.data === data){
+        return node;
+      }
+      if(node.data > data){
+        node.left = addWithin(node.left, data);
+      }
+      else{
+        node.right = addWithin(node.right, data);
+      }
+
+      return node;
+    }
+   
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has( data ) {
+    return searchWithin(this.rootVal, data);
+
+    function searchWithin(node, data){
+if(!node){
+  return false;
+}
+if(node.data === data){
+return true;
+}
+return node.data < data?
+searchWithin(node.right, data) :
+searchWithin(node.left, data)
+    }
+
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find( data ) {
+    return searchWithin(this.rootVal, data);
+    function searchWithin(node, data){
+if(!node){
+  return null;
+}
+if(node.data === data){
+return node;
+}
+return node.data < data?
+searchWithin(node.right, data) :
+searchWithin(node.left, data)
+    }
   }
 
   remove(/* data */) {
@@ -34,13 +84,25 @@ module.exports = class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   if(this.rootVal){
+     let x = this.rootVal;
+     while(x.left){
+      x = x.left;
+     }
+     return x.data;
+   }
+   return null;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.rootVal){
+      let x = this.rootVal;
+      while(x.right){
+       x = x.right;
+      }
+      return x.data;
+    }
+    return null;
   }
 
 }
